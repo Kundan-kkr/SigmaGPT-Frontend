d0a0e47ae0234ef74136055c4edbf8c1de4d4b61
Kundan-kkr <kundankkr408@gmail.com>
2026-06-04 21:19:50 +0530

    fix svg path

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,6 +1,6 @@
 import "./Sidebar.css";
 import logo from "./assets/chatgpt-icon.svg";
-import sidebar from "./assets/Sidebar.svg";
+import sidebar from "./assets/sidebar.svg";
 import { useContext, useEffect } from "react";
 import { MyContext } from "./MyContext.jsx";
 import {v1 as uuidv1} from "uuid";
